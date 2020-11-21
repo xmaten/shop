ad5f0c279abd721db2602f8a0b13c65744afcd41
Mateusz Wójcik <mateusz.wojcik@start-up.house>
2020-11-21 15:33:29 +0100

    Add meterial theme

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Head from 'next/head'
 
 type Props = {
   Component: any
@@ -6,7 +7,15 @@ type Props = {
 }
 
 const MyApp: React.FC<Props> = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
+        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
