348f9695169de5f565baf4689908ddc8330dffd7
Mateusz Wójcik <mateusz.wojcik@start-up.house>
2020-12-15 16:22:13 +0100

    Add Tailwind instead of MaterialUI

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Head from 'next/head'
-import { createMuiTheme, CssBaseline, ThemeProvider } from '@material-ui/core'
+import 'tailwindcss/tailwind.css'
+import '../styles/globals.css'
 
 type Props = {
   Component: any
@@ -8,30 +9,13 @@ type Props = {
 }
 
 const MyApp: React.FC<Props> = ({ Component, pageProps }) => {
-  const theme = createMuiTheme({
-    overrides: {
-      MuiCssBaseline: {
-        '@global': {
-          html: {
-            margin: 0,
-            padding: 0,
-            boxSizing: 'border-box',
-          },
-        },
-      },
-    },
-  })
-
   return (
     <>
       <Head>
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
         <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
       </Head>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <Component {...pageProps} />
-      </ThemeProvider>
+      <Component {...pageProps} />
     </>
   )
 }
